01bf9dff43b8c712773fd5e690997de9c9fd8f9b
Rahul Yadu <rahulyadu17@gmail.com>
2022-09-15 11:48:06 +0530

    thik hai

--- components/CoverBox.js
+++ components/CoverBox.js
@@ -1,6 +1,7 @@
 import { Box } from "@chakra-ui/react";
 
-export default function({src,name}){
+export default function 
+CoverBox({src,name}){
     return (<>
         <Box maxW='xl' p='3' m='3' className="cover-box" shadow={'dark-lg'}>
             <img src={src} ></img>
