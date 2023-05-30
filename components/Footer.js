09c24b820642d4f18c7a03e3a8750133c89a084e
Rahul Yadu <rahulyadu17@gmail.com>
2023-05-30 12:44:15 +0530

    ui - background color

--- components/Footer.js
+++ components/Footer.js
@@ -11,7 +11,7 @@ export const Footer = ()=>{
         <a href="https://twitter.com/Rahul53520475/"><i className="fa-brands fa-square-twitter fa-2x" ></i></a>
         <a href="https://www.linkedin.com/in/rahul-yadu-457168203/"><i className="fa-brands fa-linkedin fa-2x"></i></a>
         <a href="https://github.com/Rahul7yadu/"><i className="fa-brands fa-square-github fa-2x"></i></a>
-        <a href="https://mail.google.com/rahulyadu17@gmail.com"><i className="fa-solid fa-envelope fa-2x"></i></a>
+       
             </Box>
         </Box>
         </Container>
